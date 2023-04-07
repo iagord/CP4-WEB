55864f167770ab86a4472053ed3383d37e24b838
iagord <rosadiasiago@gmail.com>
2023-04-07 19:39:27 -0300

    Arrumando o código

--- Ex02.js
+++ Ex02.js
@@ -1,72 +1,82 @@
-// Crie um objeto funcionários crie um array com os atributos nome,idade, sexo, cargo, salário, descontos, dataadmissão,datademissão
 const funcionarios = [
-    {
-      nome: "Ana",
-      idade: 29,
-      sexo: "Feminino",
-      cargo: "Gerente",
-      salario: 5000,
-      descontos: 1000,
-      dataAdmissao: "2005-02-12",
-      dataDemissao: "2022-03-31"
-    },
-    {
-      nome: "Mário",
-      idade: 25,
-      sexo: "Masculino",
-      cargo: "Assistente",
-      salario: 2500,
-      descontos: 500,
-      dataAdmissao: "2010-08-20",
-      dataDemissao: "2021-11-30"
-    },
-    {
-      nome: "Paula",
-      idade: 40,
-      sexo: "Feminino",
-      cargo: "Analista",
-      salario: 3500,
-      descontos: 700,
-      dataAdmissao: "2001-10-15",
-      dataDemissao: null
-    },
-    {
-      nome: "Arthur",
-      idade: 28,
-      sexo: "Masculino",
-      cargo: "Coordenador",
-      salario: 4500,
-      descontos: 900,
-      dataAdmissao: "2012-05-10",
-      dataDemissao: null
-    },
-  ];
+  {
+    nome: 'João',
+    idade: 35,
+    sexo: 'M',
+    cargo: 'Gerente',
+    salario: 5000,
+    descontos: 1000,
+    dataAdmissao: new Date('2005-01-01'),
+    dataDemissao: null
+  },
+  {
+    nome: 'Maria',
+    idade: 28,
+    sexo: 'F',
+    cargo: 'Analista',
+    salario: 3000,
+    descontos: 500,
+    dataAdmissao: new Date('2010-03-15'),
+    dataDemissao: new Date('2022-02-28')
+  },
+  {
+    nome: 'Pedro',
+    idade: 42,
+    sexo: 'M',
+    cargo: 'Coordenador',
+    salario: 4500,
+    descontos: 800,
+    dataAdmissao: new Date('2002-11-20'),
+    dataDemissao: new Date('2020-12-31')
+  },
+  {
+    nome: 'Julia',
+    idade: 24,
+    sexo: 'F',
+    cargo: 'Desenvolvedor',
+    salario: 2500,
+    descontos: 600,
+    dataAdmissao: new Date('2015-05-10'),
+    dataDemissao: null
+  }
+];
 
-// Fazer o filter de funcionários que tiveram data de admissão entre 2000 à 2010
-const funcionariosContratados = funcionarios.filter(funcionario => {
-    const dataAdmissao = new Date(funcionario.dataAdmissao);
-    return dataAdmissao.getFullYear() >= 2000 && dataAdmissao.getFullYear() <= 2010;
-  });
-  
+// a) Fazer o filter de funcionários que tiveram data de admissão entre 2000 à 2010
+const funcionariosAdmitidosEntre2000e2010 = funcionarios.filter(funcionario => {
+  const anoAdmissao = funcionario.dataAdmissao.getFullYear();
+  return anoAdmissao >= 2000 && anoAdmissao <= 2010;
+});
 
-// Fazer um filter de funcionários que foram demitidos depois de 2018
-  const funcionariosDemitidos = funcionarios.filter(funcionario => {
-    if (funcionario.dataDemissao === null) {
-      return false; // Funcionário ainda trabalha na empresa
-    }
-    const dataDemissao = new Date(funcionario.dataDemissao);
-    return dataDemissao.getFullYear() > 2018;
-  });
+console.log('Funcionários admitidos entre 2000 e 2010:');
+console.log(funcionariosAdmitidosEntre2000e2010);
 
+// b) Fazer um filter de funcionários que foram demitidos depois de 2018
+const funcionariosDemitidosDepoisDe2018 = funcionarios.filter(funcionario => {
+  if(funcionario.dataDemissao) {
+    const anoDemissao = funcionario.dataDemissao.getFullYear();
+    return anoDemissao > 2018;
+  }
+});
 
-// Fazer um map de funcionários e trazer o nome, salário, desconto 
-  const funcionariosSimplificados = funcionarios.map(funcionario => ({
+console.log('Funcionários demitidos depois de 2018:');
+console.log(funcionariosDemitidosDepoisDe2018);
+
+// c) Fazer um map de funcionários e trazer o nome, salário, desconto
+const informacoesFuncionarios = funcionarios.map(funcionario => {
+  return {
     nome: funcionario.nome,
     salario: funcionario.salario,
-    descontos: funcionario.descontos,
-  }));
+    descontos: funcionario.descontos
+  }
+});
 
-//  Criar um Reduce para somar o valor total dos descontos 
+console.log('Informações dos funcionários:');
+console.log(informacoesFuncionarios);
+
+// d) Criar um Reduce para somar o valor total dos descontos
 const totalDescontos = funcionarios.reduce((acumulador, funcionario) => {
-    return acumulfuncionario.descontos;
-}, 0);  
+  return acumulador + funcionario.descontos;
+}, 0);
+
+console.log('Total de descontos:');
+console.log(totalDescontos);
